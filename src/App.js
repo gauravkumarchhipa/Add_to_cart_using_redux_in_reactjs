bbf705d8d4a56b2d90a6a43b7a9ec87fb2d4c511
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-03-17 13:56:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import Home from "./components/Home";
+import "./styles/app.scss"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Cart from "./components/Cart";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+
+          <Route path="*" element={<div style={{color : "white"}}>Page not found 404</div>}/>
+        </Routes>
+        <Toaster/>
+      </Router>
+    </>
   );
 }
 
